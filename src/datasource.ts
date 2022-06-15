9fda8dc4059d1789871a058cd81dc425d0a662ca
Lukas Strechel <lukas.strechel@iwu.fraunhofer.de>
2022-06-15 12:43:57 +0200

    WIP: show linkedfactory values

--- src/datasource.ts
+++ src/datasource.ts
@@ -14,9 +14,11 @@ import { MyQuery, MyDataSourceOptions, defaultQuery } from './types';
 export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
   url?: string;
   settings?: any;
+  currentUrl: string;
   constructor(instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>) {
     super(instanceSettings);
     this.url = instanceSettings.jsonData.url;
+    this.currentUrl = '';
     this.settings = instanceSettings;
   }
 
@@ -30,6 +32,7 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
     if(query.sensor) sensor = query.sensor;
 
     var buildUrl = this.url + '/values?item=' + this.url + '/' + factory + '/' + machine + '/' + sensor;
+    this.currentUrl = this.url + '/' + factory + '/' + machine + '/' + sensor;
     return buildUrl
   }
 
@@ -51,10 +54,31 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
 
       this.doRequest(query).then((res) => {
         //TODO: set values from result
-        console.log(res);
-      })
+        const frame = new MutableDataFrame({
+          refId: query.refId,
+          fields: [
+            { name: "Time", type: FieldType.time },
+            { name: "Value", type: FieldType.number },
+            { name: "Flag", type: FieldType.number },
+            { name: "A", type: FieldType.number }
+          ]
+        });
+        console.log(res.data[this.currentUrl]);
 
+        const result = res.data[this.currentUrl]
+        for(let key in result){
 
+          //access values like this
+          console.log(result['a'][0]['value']);
+          frame.appendRow([result['a']['time'], result['value']['value'], result['flag']['value'], result['a']['value']]);
+        }
+
+        console.log(frame);
+
+        return frame;
+      });
+
+      /*
       const dataFrame: MutableDataFrame = new MutableDataFrame();
       dataFrame.refId = query.refId;
       
@@ -62,9 +86,11 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
       if(query.valueSelect) dataFrame.addField({ name: 'Value', values: [query.value, query.value], type: FieldType.number });
       if(query.flagSelect) dataFrame.addField({ name: 'Flag', values: [query.flag, query.flag], type: FieldType.number });
       if(query.aSelect) dataFrame.addField({ name: 'A', values: [query.a, query.a], type: FieldType.number });
-      return dataFrame
+
+
+      return dataFrame*/
     });
-    return { data };
+    return Promise.all(data).then((data) => ({ data }));
   }
 
   async testDatasource() {
